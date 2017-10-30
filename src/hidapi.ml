d519ca7b6173a48ed34c1d0564254f0e3730af51
Vincent Bernardoff <vb@luminar.eu.org>
2017-10-30 14:27:09 +0100

    use cstruct

--- src/hidapi.ml
+++ src/hidapi.ml
@@ -14,16 +14,15 @@ type device_info = {
 } [@@deriving sexp]
 
 type hid_device
-type buffer = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
 
 external hid_init : unit -> unit = "ml_hid_init" [@@noalloc]
 external hid_exit : unit -> unit = "ml_hid_exit" [@@noalloc]
 external hid_enumerate : int -> int -> device_info list = "ml_hid_enumerate"
 external hid_open : int -> int -> hid_device = "ml_hid_open" [@@noalloc]
 external hid_open_path : string -> hid_device = "ml_hid_open_path" [@@noalloc]
-external hid_write : hid_device -> buffer -> int -> int = "ml_hid_write" [@@noalloc]
-external hid_read_timeout : hid_device -> buffer -> int -> int -> int = "ml_hid_read_timeout" [@@noalloc]
-external hid_read : hid_device -> buffer -> int -> int = "ml_hid_read" [@@noalloc]
+external hid_write : hid_device -> Cstruct.buffer -> int -> int = "ml_hid_write" [@@noalloc]
+external hid_read_timeout : hid_device -> Cstruct.buffer -> int -> int -> int = "ml_hid_read_timeout" [@@noalloc]
+external hid_read : hid_device -> Cstruct.buffer -> int -> int = "ml_hid_read" [@@noalloc]
 external hid_set_nonblocking : hid_device -> bool -> unit = "ml_hid_set_nonblocking" [@@noalloc]
 external hid_close : hid_device -> unit = "ml_hid_close" [@@noalloc]
 
@@ -33,5 +32,7 @@ let hid_enumerate ?(vendor_id=0) ?(product_id=0) () =
 let hid_open ~vendor_id ~product_id =
   hid_open vendor_id product_id
 
-let hid_read_blocking dev buf len = hid_read_timeout dev buf len (-1)
-let hid_read_timeout ~timeout dev buf len = hid_read_timeout dev buf len timeout
+let hid_write dev buf = hid_write dev (Cstruct.to_bigarray buf) (Cstruct.len buf)
+
+let hid_read ?(timeout=(-1)) dev buf len =
+  hid_read_timeout dev (Cstruct.to_bigarray buf) len timeout
--- src/hidapi.mli
+++ src/hidapi.mli
@@ -12,15 +12,13 @@ type device_info = {
 } [@@deriving sexp]
 
 type hid_device
-type buffer = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
 
 val hid_init : unit -> unit
 val hid_exit : unit -> unit
 val hid_enumerate : ?vendor_id:int -> ?product_id:int -> unit -> device_info list
 val hid_open : vendor_id:int -> product_id:int -> hid_device
 val hid_open_path : string -> hid_device
-val hid_write : hid_device -> buffer -> int -> int
-val hid_read_blocking : hid_device -> buffer -> int -> int
-val hid_read_timeout : timeout:int -> hid_device -> buffer -> int -> int
+val hid_write : hid_device -> Cstruct.t -> int
+val hid_read : ?timeout:int -> hid_device -> Cstruct.t -> int -> int
 val hid_set_nonblocking : hid_device -> bool -> unit
 val hid_close : hid_device -> unit

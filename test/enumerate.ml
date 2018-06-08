7aa807d7742aa23f852b6e62a642581ad42e7076
Vincent Bernardoff <vb@luminar.eu.org>
2018-06-08 16:17:03 +0200

    enumerate: display more info

--- test/enumerate.ml
+++ test/enumerate.ml
@@ -3,8 +3,15 @@ open Hidapi
 let () =
   init () ;
   let devs = Hidapi.enumerate () in
-  ListLabels.iter devs ~f:begin fun { vendor_id ; product_id } ->
-    Printf.printf "0x%x 0x%x\n" vendor_id product_id ;
+  ListLabels.iter devs ~f:begin fun { path; vendor_id; product_id;
+                                      serial_number; release_number;
+                                      manufacturer_string; product_string;
+                                      usage_page; usage; interface_number } ->
+    Printf.printf "%s 0x%04x 0x%04x %s %d %s %s %d\n"
+      path vendor_id product_id
+      serial_number release_number
+      manufacturer_string product_string
+      interface_number ;
     match open_id ~vendor_id ~product_id with
     | None ->
       Printf.printf "Impossible to open %d:%d\n" vendor_id product_id

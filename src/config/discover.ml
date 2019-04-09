6e9aeb0f4995824cf4444e9f9d4a2a2addcfec20
Vincent Bernardoff <vb@luminar.eu.org>
2019-04-09 13:41:29 +0200

    discover: bugfix

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -18,6 +18,6 @@ let () =
         | Some a, _
         | None, Some a -> a
     in
-    C.Flags.write_lines "c_flags.sexp" conf.cflags ;
-    C.Flags.write_lines "c_library_flags.sexp" conf.libs
+    C.Flags.write_sexp "c_flags.sexp" conf.cflags ;
+    C.Flags.write_sexp "c_library_flags.sexp" conf.libs
   end
